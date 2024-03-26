b53c057ce5ca2ae983506128184b5c24a86e1497
DianaManolache <manolache.diana27@gmail.com>
2024-03-26 14:37:52 +0200

    Added lab4

--- lab4-git/Task6/smarter-calculator/README.md
+++
@@ -1,6 +0,0 @@
-# Smarter calculator
-
-This is an example of a solution for the [task 7 of 2nd laboratory](https://teaching.blsalin.dev/pao/laborator%202).
-
-Runs with minimum version: Java 17
-Contains extra features for Java 21
--- lab4-git/Task6/smarter-calculator/src/pao/blsalin/smartercalculator/CalculationRequest.java
+++
@@ -1,9 +0,0 @@
-package pao.blsalin.smartercalculator;
-
-public record CalculationRequest(Object leftOperand, Object rightOperand, String operation) {
-    @Override
-    public String toString() {
-        return leftOperand + " " + operation + " " + rightOperand;
-    }
-
-}
